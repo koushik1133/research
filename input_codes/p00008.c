75f2117cc98fe7aacc5785977b23a56f8cfffffb
koushik1133 <koushik9924@gmail.com>
2025-06-20 01:02:06 +0530

    Add files via upload

---
+++ input_codes/p00008.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define N 16
+
+int main() {
+    int A[N][N], B[N][N], C[N][N];
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) {
+            A[i][j] = i + j;
+            B[i][j] = i - j;
+        }
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++)
+                C[i][j] += A[i][k] * B[k][j];
+        }
+
+    printf("C[0][0] = %d\n", C[0][0]);
+    return 0;
+}
